d6b208c4eb46cca036b6981215425885c40a6ff5
ken3ypa <kirie115@gmail.com>
2019-08-12 10:11:46 +0900

    chargeメソッドをRentalクラスからMovieクラスへ移動

--- lib/rental.rb
+++ lib/rental.rb
@@ -5,22 +5,11 @@ class Rental
     @movie, @days_rented = movie, days_rented
   end
 
-  def charge
-    result = 0
-    case movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
-  end
-
   def frequent_renter_points
     (movie.price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
+
+  def charge
+    movie.charge(days_rented)
   end
 end
