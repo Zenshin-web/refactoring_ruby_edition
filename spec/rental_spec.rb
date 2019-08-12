1e564bd6b23d66c8508e453afa8513822b12d015
ken3ypa <kirie115@gmail.com>
2019-08-12 11:47:47 +0900

    rental_specにテストケースを追加

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,7 +1,8 @@
 require 'rental.rb'
 
 RSpec.describe Rental do
-  let!(:movie) { Movie.new('memento', 0) }
+  let(:price_code) { Movie::REGULAR }
+  let!(:movie) { Movie.new('memento', price_code) }
   let!(:days_rented) { 7 }
   let!(:rental) { Rental.new(movie, days_rented) }
 
@@ -14,6 +15,57 @@ RSpec.describe Rental do
   describe '#days_rented' do
     it 'レンタル期間が返ること' do
       expect(rental.days_rented).to eq days_rented
+    end
+  end
+
+  describe '#charge' do
+    context '借りる映画の種類が「一般」で、貸し出し日数が2日のとき' do
+      let!(:price_code) { Movie::REGULAR }
+      let!(:days_rented) { 2 }
+
+      it 'レンタル料金が2ドルになること' do
+        # NOTE: 計算式は '2 + (days_rented - 2) * 1.5'
+        expect(rental.charge).to eq 2
+      end
+    end
+
+    context '借りる映画の種類が「一般」で、貸し出し日数が7日のとき' do
+      let!(:price_code) { Movie::REGULAR }
+      let!(:days_rented) { 7 }
+
+      it 'レンタル料金が9.5ドルになること' do
+        # NOTE: 計算式は '2 + (days_rented - 2) * 1.5'
+        expect(rental.charge).to eq 9.5
+      end
+    end
+
+    context '借りる映画の種類が「新作」のとき' do
+      let!(:price_code) { Movie::NEW_RELEASE }
+
+      it 'レンタル料金が21ドルになること' do
+        # NOTE: 計算式は 'days_rented * 3'
+        expect(rental.charge).to eq 21
+      end
+    end
+
+    context '借りる映画の種類が「子供向け」で、貸し出し日数が3日のとき' do
+      let!(:price_code) { Movie::CHILDREN }
+      let!(:days_rented) { 3 }
+
+      it 'レンタル料金が1.5ドルになること' do
+        # NOTE: 計算式は '1.5 + (days_rented - 3) * 1.5'
+        expect(rental.charge).to eq 1.5
+      end
+    end
+
+    context '借りる映画の種類が「子供向け」で、貸し出し日数が7日のとき' do
+      let!(:price_code) { Movie::CHILDREN }
+      let!(:days_rented) { 7 }
+
+      it 'レンタル料金が7.5ドルになること' do
+        # NOTE: 計算式は '1.5 + (days_rented - 3) * 1.5'
+        expect(rental.charge).to eq 7.5
+      end
     end
   end
 end
