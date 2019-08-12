6a6311b319c190fa66ecee092bb121d55978197e
ken3ypa <kirie115@gmail.com>
2019-08-12 17:33:31 +0900

    calculate_outstandingメソッドを抽出

--- cp6/extract_method.rb
+++ cp6/extract_method.rb
@@ -1,11 +1,11 @@
 def print_owing(previous_amount)
-  outstanding = previous_amount * 1.2
   print_banner
-  # calculate outstanding
-  @orders.each do |order|
-    outstanding += order.amount
-  end
+  outstanding = calculate_outstanding(outstanding * 1.2)
   print_details outstanding
+end
+
+def calculate_outstanding(initial_value)
+  @orders.inject(initial_value) { |result, order| result + order.amount }
 end
 
 def print_details(outstanding)
