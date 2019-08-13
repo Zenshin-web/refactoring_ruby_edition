c7970c31524e816bd1d62ca56d2b3e85802bfbc3
ken3ypa <kirie115@gmail.com>
2019-08-13 12:50:11 +0900

    shipping に計算式を置き換える

--- cp6/introduce_explaining_variable_sample.rb
+++ cp6/introduce_explaining_variable_sample.rb
@@ -2,7 +2,7 @@ def price
   # price is base price - quantity discount + shipping
   base_price = @quantity * @item_price
   quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
-  return base_price -
-      [quantity_discount +
-      [base_price * 0.1, 100.0].min
+  shipping = [base_price * 0.1, 100.0].min
+
+  base_price - quantity_discount + shipping
 end
