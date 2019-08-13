98c5d164d0a07a68ac4feee7d53419d0a57991a1
ken3ypa <kirie115@gmail.com>
2019-08-13 09:59:14 +0900

    descount_factor のメソッド抽出

--- cp6/replace_temp_with_query.rb
+++ cp6/replace_temp_with_query.rb
@@ -1,10 +1,9 @@
 def price
-  if base_price > 1000
-    discount_factor = 0.95
-  else
-    discount_factor = 0.98
-  end
   base_price * discount_factor
+end
+
+def discount_factor
+  base_price > 1000 ? 0.95 : 0.98
 end
 
 def base_price
