878460f99dff28ca28b89fb6a87012adf1bc0ec1
ken3ypa <kirie115@gmail.com>
2019-08-13 12:48:45 +0900

    quantity_discount に計算式を置き換える

--- cp6/introduce_explaining_variable_sample.rb
+++ cp6/introduce_explaining_variable_sample.rb
@@ -1,7 +1,8 @@
 def price
   # price is base price - quantity discount + shipping
   base_price = @quantity * @item_price
+  quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
   return base_price -
-      [0, @quantity - 500].max * @item_price * 0.05 +
+      [quantity_discount +
       [base_price * 0.1, 100.0].min
 end
